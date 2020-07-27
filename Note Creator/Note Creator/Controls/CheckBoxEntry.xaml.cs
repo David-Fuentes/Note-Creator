547479456b04a799155b66bbec7972c414ae9036
David-Fuentes <david.fuentes@faithlife.com>
2020-07-27 11:23:04 -0700

    v1.0

--- Note Creator/Note Creator/Controls/CheckBoxEntry.xaml.cs
+++ Note Creator/Note Creator/Controls/CheckBoxEntry.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using Note_Creator.Helpers;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Note_Creator.Controls
 {
@@ -24,6 +13,12 @@ namespace Note_Creator.Controls
             DataContext = this;
             BoxChecked = false;
             InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
 
         private bool m_boxChecked;
@@ -39,13 +34,7 @@ namespace Note_Creator.Controls
             set { SetValue(LabelContentProperty, value); }
         }
         public static readonly DependencyProperty LabelContentProperty =
-            DependencyProperty.Register("LabelContent", typeof(string), typeof(CheckBoxEntry), new PropertyMetadata("Change me"));
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void OnPropertyChanged(string name)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
+            DependencyProperty.Register("LabelContent", typeof(string), typeof(CheckBoxEntry), new PropertyMetadata("Adjust Label Content"));
 
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
--- Note Creator/Note Creator/Controls/TextBoxEntry.xaml.cs
+++ Note Creator/Note Creator/Controls/TextBoxEntry.xaml.cs
@@ -1,25 +1,11 @@
 ﻿using Note_Creator.Helpers;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Note_Creator.Controls
 {
-    /// <summary>
-    /// Interaction logic for TextBoxEntry.xaml
-    /// </summary>
     public partial class TextBoxEntry : UserControl, INotifyPropertyChanged
     {
         public TextBoxEntry()
@@ -29,13 +15,17 @@ namespace Note_Creator.Controls
             InitializeComponent();
         }
 
+        public event PropertyChangedEventHandler PropertyChanged;
+        private protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
         public string LabelContent
         {
             get { return (string)GetValue(LabelContentProperty); }
             set { SetValue(LabelContentProperty, value); }
         }
-        public static readonly DependencyProperty LabelContentProperty =
-            DependencyProperty.Register("LabelContent", typeof(string), typeof(TextBoxEntry), new PropertyMetadata("Please Change me"));
 
         private string m_textBoxContent;
         public string TextBoxContent
@@ -44,14 +34,16 @@ namespace Note_Creator.Controls
             set { m_textBoxContent = value; OnPropertyChanged("TextBoxContent"); }
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        private protected void OnPropertyChanged(string name)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
-
+        public static readonly DependencyProperty LabelContentProperty =
+            DependencyProperty.Register("LabelContent", typeof(string), typeof(TextBoxEntry), new PropertyMetadata("Please Change me"));
 
         private void InputBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBoxContent = InputBox.Text;
+            InputBox.Focus();
+        }
+
+        private void InputBox_LostFocus(object sender, RoutedEventArgs e)
         {
             EventHelper.OnUpdateNoteContent(this, new EventArgs());
         }
